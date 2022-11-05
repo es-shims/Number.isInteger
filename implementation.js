829c8e9ab2f8251ca07231014462784792e1081d
Jordan Harband <ljharb@gmail.com>
2022-11-04 22:50:45 -0700

    [Deps] update `define-properties`, `es-abstract`

--- implementation.js
+++ implementation.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var IsIntegralNumber = require('es-abstract/2021/IsIntegralNumber');
+var IsIntegralNumber = require('es-abstract/2022/IsIntegralNumber');
 
 module.exports = function isInteger(value) {
 	return IsIntegralNumber(value);
